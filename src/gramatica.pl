f21cfe3854a6a2b95cfc477c46772bc730d37173
sebas-mora28 <semasmora@gmail.com>
2021-03-29 22:25:59 -0600

    Agrega documentacion interna

--- src/gramatica.pl
+++ src/gramatica.pl
@@ -1,4 +1,8 @@
 
+
+
+
+%---- PRONOMBRES --------------
 pronombre([me|S],S).
 pronombre([mi|S],S).
 pronombre([yo|S],S).
@@ -6,16 +10,19 @@ pronombre([nosotros|S],S).
 pronombre([nos|S],S).
 pronombre([se|S], S).
 
+%---- ARTICULOS --------------
 articulo([la|S],S).
 articulo([el|S],S).
 articulo([al|S],S).
 
+
+%---- PREPOSICIONES --------------
 preposicion([en|S],S).
 preposicion([para|S],S).
 preposicion([hacia|S],S).
 preposicion([a|S],S).
 
-
+%---- VERBOS --------------
 verbo([estoy|S],S).
 verbo([estamos|S],S).
 verbo([esta|S], S).
@@ -27,10 +34,13 @@ verbo([ir|S],S).
 verbo([quiero|S],S).
 verbo([pasar|S],S).
 
-
+%---- NEGACION --------------
 negacion([no|[]],[]).
 negacion([no|_]).
 
+
+
+%---- ESTABLECIMIENTO --------------
 establecimiento([supermercado|_]).
 establecimiento([farmacia|_]).
 establecimiento([mercado|_]).
@@ -39,17 +49,46 @@ establecimiento([zapateria|_]).
 
 
 arco(cartago, atenas).
+
+/**
+ * Nombre: localizacion 
+ * Descripción: Llama la cláusula existe_lugar.
+ */
 localizacion([Lugar|[]]):- existe_lugar(Lugar).
 
+
+/**
+ * Nombre: existe_lugar
+ * Descripción: Verifica si un lugar dado existe en la base de datos.
+ */
+existe_lugar(Lugar):- arco(Lugar,_); arco(_,Lugar).
+
+
+/**
+ * Nombre: oracion
+ * Descripción: Define la estructura de las oraciones permitidas por la grámatica libre de contexto. 
+ */
 oracion(S0, S):- sintagma_verbal(S0, S), localizacion(S).
 oracion(S0, S):- sintagma_nominal(S0, S1), sintagma_verbal(S1, S), localizacion(S).
 oracion(S0, S):- sintagma_nominal(S0, S1), sintagma_verbal(S1,S2), sintagma_nominal(S2,S).
 oracion(S0, S):- sintagma_verbal(S0,S1), sintagma_nominal(S1,S).
 oracion(S0, S):- sintagma_nominal(S0, S).
 
+
+
+/**
+ * Nombre: sintagma_verbal
+ * Descripción: Define las posibles estructuras permitidas de los signtamas verbales 
+ */
 sintagma_verbal(S0, S):- verbo(S0,S1), preposicion(S1,S).
 sintagma_verbal(S0,S):- verbo(S0, S1), verbo(S1,S).
 sintagma_verbal(S0, S):- verbo(S0,S1), verbo(S1, S2), preposicion(S2,S).
+
+
+/**
+ * Nombre: sintagma_nominal
+ * Descripción: Define las posibles estructuras permitidas de los signtamas nominales
+ */
 sintagma_nominal(S0, S):- pronombre(S0,S).
 sintagma_nominal(S0, S):- negacion(S0,S).
 sintagma_nominal(S0,S):- articulo(S0, S), establecimiento(S).
--- src/wazelog.pl
+++ src/wazelog.pl
@@ -1,45 +1,105 @@
 #!/usr/local/bin/swipl
 :-consult(gramatica).
 
+
+
+
+/**
+ * Nombre: wazelog. 
+ * Descripcion: Funcion principal, inicializa la aplicacion, llama a las cláusulas para pedir el origen, los destinos 
+ *              intermedios y el destino final.
+ * 
+ */
 wazelog:- bienvenida,
           repeat, 
-            preguntar_origen(Origen),
+            preguntar_origen(Origen), 
             preguntar_intermedio(Intermedios),
             preguntar_destino(Destino), write(Intermedios), write(Origen), write(Destino).
         
 
-% --------------------- Clausulas generales ------------------------------------------------------
+% --------------------- Cláusulas generales ------------------------------------------------------
 
+
+/**
+ * Nombre: bienvenida
+ * Descripcion: Imprime un mensaje de bienvenida.
+ *      
+ */
 bienvenida:- write("Wazelog: Bienvenido a Wazelog, la mejor lógica para llegar a su destino"), nl.
 
+
+
+/**
+ * Nombre: error
+ * Descripcion: Imprime un mensaje de error.
+ *      
+ */
 error:- write("Disculpa, no te he entendido o el lugar que ingresaste no existe").
 
-existe_lugar(Lugar):- arco(Lugar,_); arco(_,Lugar).
-existe_lugar(_):- write("El lugar que ingresaste no existe"),!,fail.
+
+
+/**
+ * Nombre: existe_establecimiento
+ * Descripcion: Verifica si existe un establecimiento dado.
+ *      
+ */
 existe_establecimiento(Establecimiento):- establecimiento([Establecimiento]).
 
 
+
+/**
+ * Nombre: respuesta_usuario
+ * Descripcion: Verifica si la respuesta dada por el usuario cumple con las reglas establecidas en la grámatica
+ *              libre de contexto.
+ *      
+ */
 respuesta_usuario(Respuesta, Lugar):- oracion(Respuesta, [Lugar|_]).
 respuesta_usuario(Respuesta):- oracion(Respuesta, _).
 
+
+
 % ----------------- ORIGEN ------------------------------------------------------- 
 
+
+/**
+ * Nombre: preguntar_origen
+ * Descripcion: Se encarga de pedir el lugar de origen al usario, una vez obtenido el lugar verifica que
+ *              efectivamente existe dentro de la base de datos.
+ */
 preguntar_origen(Origen):- write("Wazelog: Por favor indique donde se encuentra"), nl,
                    write("Usuario: "),readln(Respuesta),
                    respuesta_usuario(Respuesta, Origen),
                    existe_lugar(Origen),!.
 
 
+/**
+ * Nombre: preguntar_origen
+ * Descripcion: Se encarga de mandar un mensaje de error en caso de no recibir una oracion correcta y volver a llamar 
+ *              la cláusula con el mismo nombre para volver a pedir el lugar de origen.
+ */
 preguntar_origen(Origen):- error, nl, preguntar_origen(Origen).
 
 
 
 % ----------------- INTERMEDIO -----------------------------------------------------
 
+
+/**
+ * Nombre: preguntar_intermedio
+ * Descripcion: Se encarga de preguntar si el usuario tiene algún destino intermedio y recibir su respuesta. Llama a las cláusla de
+ *              su mismo nombre para verificar la respuesta. Devuelve una lista con los destinos intermedios dados por el usuario.
+ *      
+ */
 preguntar_intermedio(Intermedios):- write("Wazelog: ¿Tiene algún destino intermedio?"),nl,
                                     write("Usuario: "), readln(Respuesta), preguntar_intermedio(Respuesta, Intermedios).
 
-
+/**
+ * Nombre: preguntar_intermedio
+ * Descripcion: Se encarga de recibir la respuesta del usuario, primeramente valida que la respuesta cumpla con la gramática, verfica que 
+ *              el establecimiento dado por el usuario existe. Después obtiene y verifica la respuesta del usuario a la pregunta ¿Dónde se encuentra el
+*              establecimiento?. Forma una lista con todos aquellos destinos intermedios que el usuario agregó.
+ *      
+ */
 preguntar_intermedio(Respuesta, [Lugar | Intermedios]):-  respuesta_usuario(Respuesta,Establecimiento), dif(Respuesta, no),
                                                 existe_establecimiento(Establecimiento),
                                                 write("Wazelog: ¿Dónde se encuentra el/la "), write(Establecimiento), write("?"),nl,
@@ -49,18 +109,42 @@ preguntar_intermedio(Respuesta, [Lugar | Intermedios]):-  respuesta_usuario(Resp
                                                 preguntar_intermedio(Intermedios).
 
 
+
+/**
+ * Nombre: preguntar_intermedio
+ * Descripcion: Verifica si la respuesta a la pregunta '¿Tiene algún destino intermedio?' sea negativa, para salir del loop .
+ */
 preguntar_intermedio(Respuesta, []):- respuesta_usuario(Respuesta), negacion(Respuesta), !.
 
 
+/**
+ * Nombre: preguntar_intermedio
+ * Descripcion: Se encarga de mandar un mensaje de error en caso de no recibir una oracion correcta y volver a llamar 
+ *              la cláusula con el mismo nombre para volver a pedir un destino intermedio.
+ */
 preguntar_intermedio(_, Intermedios):- error, nl,  preguntar_intermedio(Intermedios).
+
+
+
+
 
 %----------------------------- DESTINO ----------------------------------------------
 
+/**
+ * Nombre: preguntar_destino
+ * Descripcion: Se encarga de pedir el lugar de destino al usario, una vez obtenido el lugar verifica que
+ *              efectivamente existe dentro de la base de datos.
+ */
 preguntar_destino(Destino):- write("Wazelog: ¿Cuál es su destino?"), nl,
                     write("Usuario: "),readln(Respuesta),
-                    respuesta_usuario(Respuesta, Destino).
+                    respuesta_usuario(Respuesta, Destino),
+                    existe_lugar(Destino),!.
 
-
+/**
+ * Nombre: preguntar_destino
+ * Descripcion: Se encarga de mandar un mensaje de error en caso de no recibir una oracion correcta y volver a llamar 
+ *              la cláusula con el mismo nombre para volver a pedir el lugar de destino.
+ */                
 preguntar_destino(Destino):- error, nl, preguntar_destino(Destino).
 
 
