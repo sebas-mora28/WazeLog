ade7d93c88d830a1100ec0d8bdde2600c227ed31
sebas-mora28 <semasmora@gmail.com>
2021-04-04 17:25:37 -0600

    Añade nuevas estructuras de oraciones

--- src/gramatica.pl
+++ src/gramatica.pl
@@ -14,10 +14,15 @@ pronombre([el|S],S).
 pronombre([ella|S],S).
 
 
+pronombre_reflexivo([se|S], S).
+pronombre_relativo([que|S], S).
+
+
 %---- ARTICULOS --------------
 articulo([la|S],S).
 articulo([el|S],S).
 articulo([al|S],S).
+
 
 
 %---- PREPOSICIONES --------------
@@ -32,17 +37,22 @@ verbo([estamos|S],S).
 verbo([esta|S], S).
 verbo([voy|S],S).
 verbo([va|S],S).
-verbo([van]|S), S.
+verbo([van|S], S).
 verbo([vamos|S],S).
 verbo([dirijo|S],S).
-verbo([dirigimos|S],S).
+verbo([dirijen|S],S).
+verbo([dirijimos|S],S).
 verbo([ir|S],S).
 verbo([quiero|S],S).
 verbo([pasar|S],S).
 verbo([ubicado|S], S).
 verbo([ubica|S], S).
 verbo([ubico|S], S).
+verbo([ubican|S], S).
 verbo([encuentra|S], S).
+verbo([tengo|S], S).
+
+
 %---- NEGACION --------------
 negacion([no|[]],[]).
 negacion([no|_]).
@@ -88,7 +98,10 @@ oracion(S0, S):- sintagma_nominal(S0, S).
  * Descripción: Define las posibles estructuras permitidas de los signtamas verbales 
  */
 sintagma_verbal(S0, S):- verbo(S0,S1), preposicion(S1,S).
+sintagma_verbal(S0, S):- verbo(S0,S1), pronombre_relativo(S1, S2), verbo(S2, S3), preposicion(S3,S).
+sintagma_verbal(S0, S):- verbo(S0,S1), pronombre_relativo(S1, S2), verbo(S2, S).
 sintagma_verbal(S0,S):- verbo(S0, S1), verbo(S1,S).
+sintagma_verbal(S0,S):- verbo(S0, S).
 sintagma_verbal(S0, S):- verbo(S0,S1), verbo(S1, S2), preposicion(S2,S).
 
 
@@ -97,5 +110,6 @@ sintagma_verbal(S0, S):- verbo(S0,S1), verbo(S1, S2), preposicion(S2,S).
  * Descripción: Define las posibles estructuras permitidas de los signtamas nominales
  */
 sintagma_nominal(S0, S):- pronombre(S0,S).
+sintagma_nominal(S0, S):- pronombre(S0,S1), pronombre_reflexivo(S1, S).
 sintagma_nominal(S0, S):- negacion(S0,S).
 sintagma_nominal(S0,S):- articulo(S0, S), establecimiento(S).
